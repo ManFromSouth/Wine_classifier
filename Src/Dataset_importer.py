07956fea68bed98e733f421b4d140d27bbb3b6ea
ManfromSouth <igor.bordonos@gmail.com>
2020-06-11 19:44:58 +0300

    Done: importing dataset, model fitting, model predictions

---
+++ Src/Dataset_importer.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+
+def import_reds():
+    dataset_red_inputs = list()
+    dataset_red_outputs = list()
+    with open('Dataset_red.txt', encoding='UTF') as f:
+        for line in f:
+            data_line = line.split('\t')
+            # перевод текстовых выражених числовых переменных в собственно числа
+            for number, item in enumerate(data_line[:15]):
+                data_line[number] = float(item)
+            # отрубание перехода на следующую строку в последнем элементе
+            data_line[16] = data_line[16][:-1]
+            # добавления вектора входов
+            dataset_red_inputs.append(data_line[:15])
+            # добавление вектора выходов
+            dataset_red_outputs.append(data_line[15:])
+    return np.array(dataset_red_inputs), np.array(dataset_red_outputs)
+
+
+def import_whites():
+    dataset_white_inputs = list()
+    dataset_white_outputs = list()
+    # методы тербуют, чтобы метки были заменены числами
+    type_map = dict()
+    zone_map = dict()
+    with open('Dataset_white.txt', encoding='UTF') as f:
+        for line in f:
+            data_line = line.split('\t')
+            # перевод текстовых выражених числовых переменных в собственно числа
+            for number, item in enumerate(data_line[:15]):
+                data_line[number] = float(item)
+            # отрубание перехода на следующую строку в последнем элементе
+            data_line[16] = data_line[16][:-1]
+            # добавления вектора входов
+            dataset_white_inputs.append(data_line[:15])
+            # добавление вектора выходов
+            dataset_white_outputs.append(data_line[15:])
+    return np.array(dataset_white_inputs), np.array(dataset_white_outputs),
+
+
+if __name__ == '__main__':
+    print(import_reds()[1])
+    print(import_whites()[1])
--- Src/Models.py
+++ Src/Models.py
@@ -1,6 +1,10 @@
+from sklearn.base import clone
+from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC, NuSVC
+
+from Src.Dataset_importer import import_whites, import_reds
 
 
 class Models(object):
@@ -12,9 +16,14 @@ class Models(object):
         # red_svm_type, red_svm_zone,
         # red_nbc_type, red_nbc_zone,
         # red_knn_type, red_knn_zone,
+        # либо both
         # если для обоих типов классификации используются одинаковые модели,
         # то указатели на такую модель могут быть в обоихк ключах
         self.model_index = dict()
+        self.white_input_set = None
+        self.white_output_set = None
+        self.red_input_set = None
+        self.red_output_set = None
 
     # назначет соответствующие модели в индекс
     # model_type может быть только: svm - метод опорных веторов
@@ -29,7 +38,7 @@ class Models(object):
             self.model_index['{}_{}_type'.format(wine_type, model_type)] = model_object
         elif dep_variable == 'оба':
             self.model_index['{}_{}_zone'.format(wine_type, model_type)] = model_object
-            self.model_index['{}_{}_type'.format(wine_type, model_type)] = model_object
+            self.model_index['{}_{}_type'.format(wine_type, model_type)] = clone(model_object)
         else:
             # TODO: сюда запихать обработчик ошибок
             pass
@@ -85,6 +94,37 @@ class Models(object):
         models = self.__create_nbc()
         for variable, model in models.items():
             self.__assign_model('red', variable, 'nbc', model)
+
+    def load_datasets(self):
+        self.white_input_set, self.white_output_set = import_whites()
+        self.red_input_set, self.red_output_set = import_reds()
+
+    def fit_models(self):
+        # разбиенеие на тестовые и тренировочные выборки
+        white_train_inps, white_test_inps, white_train_outs, white_test_outs = train_test_split(self.white_input_set,
+                                                                                                self.white_output_set,
+                                                                                                random_state=1000)
+        red_train_inps, red_test_inps, red_train_outs, red_test_outs = train_test_split(self.red_input_set,
+                                                                                        self.red_output_set,
+                                                                                        random_state=1000)
+        for method in ['svm', 'knn', 'nbc']:
+            # белые
+            self.model_index['white_{}_type'.format(method)].fit(white_train_inps, white_train_outs[:, 0])
+            self.model_index['white_{}_zone'.format(method)].fit(white_train_inps, white_train_outs[:, 1])
+            # красные
+            self.model_index['red_{}_type'.format(method)].fit(red_train_inps, red_train_outs[:, 0])
+            self.model_index['red_{}_zone'.format(method)].fit(red_train_inps, red_train_outs[:, 1])
+
+    # возвращает словарь состоящий из результатов разынх моделей на обоих классификациях
+    def predict(self, wine_type, inputs):
+        inp_cor = inputs.reshape(1, -1)
+        results = dict()
+        for method in ['svm', 'knn', 'nbc']:
+            results['{}_type'.format(method)] = self.model_index['{}_{}_type'.format(wine_type, method)].predict(
+                inp_cor)
+            results['{}_zone'.format(method)] = self.model_index['{}_{}_zone'.format(wine_type, method)].predict(
+                inp_cor)
+        return results
 
     # создает объект классификационной модели метода опорных векторов
     # model_data - словарь с необходимыми данными для постройки модели
@@ -155,3 +195,5 @@ if __name__ == '__main__':
     test_models.load_models(None)
     for model_type, model in test_models.model_index.items():
         print(model_type, model)
+    test_models.load_datasets()
+    test_models.fit_models()
