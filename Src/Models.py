0390571b09e11d9abcbb7543adfa408c333c71e6
ManFromSouth <igor.bordonos@gmail.com>
2021-06-22 10:11:32 +0300

    Добавлена обработка исключительной ситуации при использовании некорректной модели SVM

--- Src/Models.py
+++ Src/Models.py
@@ -3,7 +3,7 @@ from sklearn.utils import _cython_blas
 from sklearn.base import clone
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import typedefs, KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC, NuSVC
 
 from Dataset_importer import import_whites, import_reds
